66e82fbf065c01a7942e324e3c19a53ca2b9ea76
Damian <damianmarti@gmail.com>
2024-07-31 15:17:52 -0300

    Add connect button

--- packages/nextjs/app/submit/_component/SubmitButton.tsx
+++ packages/nextjs/app/submit/_component/SubmitButton.tsx
@@ -2,21 +2,25 @@
 
 import { useFormStatus } from "react-dom";
 import { useAccount } from "wagmi";
+import { RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 
 // To use useFormStatus we need to make sure button is child of form
 const SubmitButton = () => {
   const { pending } = useFormStatus();
   const { isConnected } = useAccount();
-  const isSubmitDisabled = !isConnected || pending;
 
   return (
     <div
       className={`flex ${!isConnected && "tooltip tooltip-bottom"}`}
       data-tip={`${!isConnected ? "Please connect your wallet" : ""}`}
     >
-      <button className="btn btn-primary w-full" disabled={isSubmitDisabled} aria-disabled={isSubmitDisabled}>
-        Submit
-      </button>
+      {isConnected ? (
+        <button className="btn btn-primary w-full" disabled={pending} aria-disabled={pending}>
+          Submit
+        </button>
+      ) : (
+        <RainbowKitCustomConnectButton fullWidth={true} />
+      )}
     </div>
   );
 };
