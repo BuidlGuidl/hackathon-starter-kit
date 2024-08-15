3947c02fb2752d0ecb09cc623a99915cac7a49c6
Shiv Bhonde <shivbhonde04@gmail.com>
2024-08-15 15:45:26 +0900

    use pixel height for section

--- packages/nextjs/components/extensions-hackathon/Timeline.tsx
+++ packages/nextjs/components/extensions-hackathon/Timeline.tsx
@@ -1,8 +1,8 @@
 export const Timeline = () => {
   return (
-    <div className="flex flex-col w-full min-h-[120vh] lg:min-h-fit bg-accent items-center lg:justify-between border border-1 border-t-0 border-black p-12">
+    <div className="flex flex-col w-full min-h-[120vh] lg:min-h-[400px] gap-3 lg:gap-5 bg-accent items-center border border-1 border-t-0 border-black p-12">
       <h1 className="text-4xl">Timeline</h1>
-      <div className="min-w-full lg:mt-32 flex-1 flex">
+      <div className="min-w-full lg:mt-32 flex flex-1 lg:flex-grow-0">
         <div className="flex flex-col items-center lg:flex-row justify-center gap-3 lg:gap-5 flex-1">
           <div className="h-[80%] w-[20px] lg:w-[80%] lg:h-[20px]  bg-base-200 flex justify-between relative">
             <div className="flex flex-col space-y-2 items-center absolute rotate-90 text-center lg:rotate-0 lg:-top-[68px] lg:left-[10%]">
