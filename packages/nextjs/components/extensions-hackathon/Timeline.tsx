0d79dfe3fdf3cfa2612e66d1eb423aaa61261602
Shiv Bhonde <shivbhonde04@gmail.com>
2024-08-15 14:40:25 +0900

    WIP: horizontal basic poc

--- packages/nextjs/components/extensions-hackathon/Timeline.tsx
+++ packages/nextjs/components/extensions-hackathon/Timeline.tsx
@@ -4,7 +4,25 @@ export const Timeline = () => {
       <h1 className="text-4xl">Timeline</h1>
       <div className="min-w-full mt-32">
         <div className="flex justify-center space-x-5">
-          <div className="w-[80%] bg-base-200  min-h-[20px]"></div>
+          <div className="w-[80%] bg-base-200 h-[20px] flex justify-between relative">
+            <div className="flex flex-col space-y-2 items-center absolute -top-[68px] left-[10%]">
+              <h2 className="m-0">Application open</h2>
+              <p className="m-0 font-bold mb-4">Aug 21</p>
+              <div className="!mt-8 bg-black min-w-[28px] min-h-[1px] rotate-90"></div>
+            </div>
+
+            <div className="flex flex-col space-y-2 items-center absolute -top-[68px] left-[45%]">
+              <h2 className="m-0">Application open</h2>
+              <p className="m-0 font-bold">Aug 23</p>
+              <div className="!mt-8 bg-black min-w-[28px] min-h-[1px] rotate-90"></div>
+            </div>
+
+            <div className="flex flex-col space-y-2 items-center absolute -top-[68px] right-[10%]">
+              <h2 className="m-0">Application open</h2>
+              <p className="m-0 font-bold">Aug 24</p>
+              <div className="!mt-8 bg-black min-w-[28px] min-h-[1px] rotate-90"></div>
+            </div>
+          </div>
           <div className="w-[20px] bg-base-200  min-h-[20px]"></div>
           <div className="w-[10px] bg-base-200  min-h-[20px]"></div>
         </div>
