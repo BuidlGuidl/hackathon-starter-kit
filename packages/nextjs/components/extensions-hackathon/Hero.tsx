d5a3840d2410c2aac1758b0e9baa313c137dba91
Shiv Bhonde <shivbhonde04@gmail.com>
2024-08-15 13:33:57 +0900

    use font-spaceMono in ScaffoldETHProvider wrapper

--- packages/nextjs/components/extensions-hackathon/Hero.tsx
+++ packages/nextjs/components/extensions-hackathon/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <div className="flex flex-col pt-8 md:pt-0 md:flex-row w-full md:h-[60vh] font-spaceMono font-normal justify-between border border-1 border-black">
+    <div className="flex flex-col pt-8 md:pt-0 md:flex-row w-full md:h-[60vh] font-normal justify-between border border-1 border-black">
       <div className="flex flex-col items-start justify-center ml-6 md:ml-14 w-1/2">
         <Image src="/se-2-logo.svg" alt="Hero" width={200} height={200} />
         <h1 className="text-4xl md:text-4xl lg:text-7xl 2xl:text-8xl">
--- packages/nextjs/components/extensions-hackathon/PrizeInfo.tsx
+++ packages/nextjs/components/extensions-hackathon/PrizeInfo.tsx
@@ -1,6 +1,6 @@
 export const PrizeInfo = () => {
   return (
-    <div className="flex w-full h-[20vh] md:h-[30vh] bg-accent font-spaceMono justify-between border border-1 border-t-0 border-black">
+    <div className="flex w-full h-[20vh] md:h-[30vh] bg-accent justify-between border border-1 border-t-0 border-black">
       <div className="flex flex-col items-center justify-center w-full">
         <h2 className="text-5xl md:text-7xl">5,000$</h2>
         <p className="text-2xl md:text-3xl text-center">Available in prizes</p>
