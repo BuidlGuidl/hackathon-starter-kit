e00417e4d36fb31d4fb2beb9c4f47ab7b67bc7f6
Carlos Sánchez <oceanrdn@gmail.com>
2024-08-16 17:42:15 +0200

    Metas Update

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -6,8 +6,8 @@ import "~~/styles/globals.css";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
-  title: "Scaffold-ETH 2 App",
-  description: "Built with 🏗 Scaffold-ETH 2",
+  title: "SE-2 Extensions Hackathon",
+  description: "Build your custom Scaffold-ETH 2 extension and compete for prizes!",
 });
 
 const spaceMono = Space_Mono({
