a5649ffef56ba015b4a0c4c48ca8c9b7e71af00e
Pablo Alayeto <55535804+Pabl0cks@users.noreply.github.com>
2024-08-18 01:45:36 +0200

    Add hackathon "rules" to submission page

--- packages/nextjs/app/submit/page.tsx
+++ packages/nextjs/app/submit/page.tsx
@@ -12,10 +12,13 @@ const Submit: NextPage = () => {
           easy to use, and solve a real problem.
         </p>
         <h3 className="text-xl underline">Please pay attention to the following before submitting</h3>
-        <p className="text-lg">
-          A good extension is a useful tool that helps developers to build better apps. It should be well-documented,
-          easy to use, and solve a real problem.
-        </p>
+        <ul className="list-disc pl-5 text-lg mt-4">
+          <li>Follow extension guidelines to ensure compatibility with Scaffold-ETH 2 CLI.</li>
+          <li>Submit only your original work, disclose any pre-existing work used.</li>
+          <li>Be respectful to all participants.</li>
+          <li>Focus on quality, including documentation and demo video.</li>
+          <li>Limit of 5 submissions per person.</li>
+        </ul>
       </div>
       <div className="w-[100%] md:w-7/12 p-4 md:p-12 bg-accent">
         <div className="flex items-center flex-col flex-grow">
