b964ed7a631e19f637b536f00fcca50905e7063d
Pablo Alayeto <55535804+Pabl0cks@users.noreply.github.com>
2024-08-18 01:46:44 +0200

    Small tweaks: spacing, border color, title

--- packages/nextjs/app/submit/page.tsx
+++ packages/nextjs/app/submit/page.tsx
@@ -3,11 +3,11 @@ import { NextPage } from "next";
 
 const Submit: NextPage = () => {
   return (
-    <div className="flex flex-col md:flex-row m-6 md:m-10 border-2">
-      <div className="w-[100%] md:w-5/12 border-r-2 p-4 md:p-12">
-        <h2 className="text-2xl md:text-3xl mb-16">Before submitting</h2>
+    <div className="flex flex-col md:flex-row m-6 md:m-10 border border-black">
+      <div className="w-[100%] md:w-5/12 border-r border-black p-4 md:p-12">
+        <h2 className="text-2xl md:text-3xl mb-8">Before submitting</h2>
         <h3 className="text-xl underline">What makes a good extension</h3>
-        <p className="text-lg">
+        <p className="text-lg mb-8">
           A good extension is a useful tool that helps developers to build better apps. It should be well-documented,
           easy to use, and solve a real problem.
         </p>
@@ -22,7 +22,7 @@ const Submit: NextPage = () => {
       </div>
       <div className="w-[100%] md:w-7/12 p-4 md:p-12 bg-accent">
         <div className="flex items-center flex-col flex-grow">
-          <h1 className="text-2xl md:text-3xl mb-12">Submit your project</h1>
+          <h1 className="text-2xl md:text-3xl mb-8">Submit your extension</h1>
           <Form />
         </div>
       </div>
