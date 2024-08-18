e16583485273e599157c9cefb5984d7ed9c20962
Pablo Alayeto <55535804+Pabl0cks@users.noreply.github.com>
2024-08-19 00:47:52 +0200

    Add real prizes and ENS collaboration

--- packages/nextjs/components/extensions-hackathon/PrizeInfo.tsx
+++ packages/nextjs/components/extensions-hackathon/PrizeInfo.tsx
@@ -1,9 +1,13 @@
 export const PrizeInfo = () => {
   return (
-    <div className="flex w-full h-[20vh] md:h-[30vh] bg-accent justify-between border border-1 border-t-0 border-black">
-      <div className="flex flex-col items-center justify-center w-full">
-        <h2 className="text-5xl md:text-7xl">5,000$</h2>
-        <p className="text-2xl md:text-3xl text-center">Available in prizes</p>
+    <div className="flex flex-col md:flex-row w-full h-auto md:h-[30vh] bg-accent border border-1 border-t-0 border-black">
+      <div className="flex flex-col items-center justify-center w-full md:w-2/3 py-4 md:py-0 md:border-r border-black">
+        <h2 className="text-5xl md:text-7xl">$10,000</h2>
+        <p className="text-2xl md:text-3xl text-center mt-2">Available in prizes</p>
+      </div>
+      <div className="flex flex-col items-center justify-center w-full md:w-1/3 pt-0 pb-4 md:py-0 border-t md:border-t-0 border-black">
+        <p className="text-lg md:text-xl 2xl:text-2xl text-center mb-1 md:mb-2">In collaboration with</p>
+        <img src="/ens-logo-dao.png" alt="ENS DAO Logo" className="w-20 md:w-32" />
       </div>
     </div>
   );
