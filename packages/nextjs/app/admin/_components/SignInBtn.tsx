43555a43217c9f9dd475b0dd373c838e8bbacd31
Shiv Bhonde <shivbhonde04@gmail.com>
2024-08-19 15:07:17 +0900

    refresh the router after connected

--- packages/nextjs/app/admin/_components/SignInBtn.tsx
+++ packages/nextjs/app/admin/_components/SignInBtn.tsx
@@ -1,9 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
+import { useAuthSession } from "~~/hooks/useAuthSession";
 
 export const SignInBtn = () => {
   const { openConnectModal } = useConnectModal();
+  const { isAuthenticated } = useAuthSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      router.refresh();
+    }
+  }, [router, isAuthenticated]);
+
   return (
     <button className="btn btn-outline text-lg font-normal" onClick={openConnectModal} type="button">
       Sign in with Ethereum
