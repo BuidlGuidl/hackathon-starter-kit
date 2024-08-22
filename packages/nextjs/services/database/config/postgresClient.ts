5d868bee97eae5e2cce042b598f87b3918d48b3a
Shiv Bhonde <shivbhonde04@gmail.com>
2024-08-22 17:47:30 +0900

    use appropriate drive based on env

--- packages/nextjs/services/database/config/postgresClient.ts
+++ packages/nextjs/services/database/config/postgresClient.ts
@@ -1,12 +1,24 @@
 import * as schema from "./schema";
+import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Client } from "pg";
+import { drizzle as drizzleVercel } from "drizzle-orm/vercel-postgres";
+import { Pool } from "pg";
 
-const client = new Client({
-  connectionString: process.env.POSTGRES_URL,
-});
+let db: ReturnType<typeof drizzle<typeof schema>> | ReturnType<typeof drizzleVercel<typeof schema>>;
 
-await client.connect();
-const db = drizzle(client, { schema });
+if (process.env.NODE_ENV === "production" || process.env.VERCEL_ENV === "preview") {
+  db = drizzleVercel(sql, { schema });
+} else {
+  const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+  });
+
+  db = drizzle(pool, { schema });
+
+  pool.on("error", err => {
+    console.error("Unexpected error on idle client", err);
+    process.exit(-1);
+  });
+}
 
 export { db };
