40616673dcbe9b6866d3b0ce767af8e6aed18e10
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-08-25 10:22:14 +0800

    adding tabs to separate voted submissions

--- packages/nextjs/app/admin/_components/Submissions.tsx
+++ packages/nextjs/app/admin/_components/Submissions.tsx
@@ -3,13 +3,36 @@ import { getAllSubmissions } from "~~/services/database/repositories/submissions
 
 export const Submissions = async () => {
   const submissions = await getAllSubmissions();
+  const submissionsWithVotes = submissions.filter(submission => submission.votes.length > 0);
+  const submissionsWithoutVotes = submissions.filter(submission => submission.votes.length === 0);
 
   return (
     <div className="px-6">
-      <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
-        {submissions?.map(submission => {
-          return <SubmissionCard key={submission.id} submission={submission} />;
-        })}
+      <div role="tablist" className="tabs tabs-bordered tabs-lg bg-white">
+        <input
+          type="radio"
+          name="submission_tabs"
+          role="tab"
+          className="tab whitespace-nowrap"
+          aria-label="Not Voted"
+          defaultChecked
+        />
+        <div role="tabpanel" className="tab-content py-6">
+          <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
+            {submissionsWithoutVotes?.map(submission => {
+              return <SubmissionCard key={submission.id} submission={submission} />;
+            })}
+          </div>
+        </div>
+
+        <input type="radio" name="submission_tabs" role="tab" className="tab whitespace-nowrap" aria-label="Voted" />
+        <div role="tabpanel" className="tab-content py-6">
+          <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
+            {submissionsWithVotes?.map(submission => {
+              return <SubmissionCard key={submission.id} submission={submission} />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
