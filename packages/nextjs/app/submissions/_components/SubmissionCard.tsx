118ee69e9ba22ee006bf1eb98e8c9f1ec59915fa
Damian <damianmarti@gmail.com>
2024-09-04 16:58:03 -0300

    Add submissions page

---
+++ packages/nextjs/app/submissions/_components/SubmissionCard.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Address } from "~~/components/scaffold-eth";
+import { Submission } from "~~/services/database/repositories/submissions";
+
+export const SubmissionCard = ({ submission }: { submission: Submission }) => {
+  return (
+    <div key={submission.id} className="card bg-base-200 text-secondary-content border border-gray-300 rounded-none">
+      <div className="card-body p-4 pt-6">
+        <h2 className="card-title mb-3 xl:text-2xl">{submission.title}</h2>
+        <div className="flex flex-wrap justify-between items-center gap-4">
+          <div className="mt-1 flex shrink-0 gap-3">
+            {submission.linkToRepository && (
+              <a href={submission.linkToRepository} className="inline-block" target="_blank">
+                <img alt="github icon" className="w-6 h-6" src="/icon-github.svg" />
+              </a>
+            )}
+
+            <a href={submission.linkToVideo} className="inline-block" target="_blank">
+              <img alt="youtube icon" className="w-6 h-6" src="/icon-youtube.svg" />
+            </a>
+          </div>
+
+          {submission.builder && <Address address={submission.builder} />}
+        </div>
+
+        <p>{submission.description}</p>
+      </div>
+    </div>
+  );
+};
---
+++ packages/nextjs/app/submissions/page.tsx
@@ -0,0 +1,24 @@
+import { SubmissionCard } from "./_components/SubmissionCard";
+import type { NextPage } from "next";
+import { getAllSubmissions } from "~~/services/database/repositories/submissions";
+
+const Admin: NextPage = async () => {
+  const submissions = await getAllSubmissions();
+
+  return (
+    <div className="max-w-7xl container mx-auto px-6">
+      <div className="grid gap-6 md:grid-cols-2 xl:grid-cols-3">
+        {submissions.length === 0 && (
+          <div role="alert" className="alert col-span-2">
+            <span>No submissions yet.</span>
+          </div>
+        )}
+        {submissions.map(submission => {
+          return <SubmissionCard key={submission.id} submission={submission} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Admin;
